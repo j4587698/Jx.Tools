4156c22cc58595d4e1cc4da51af61b3cc60098dd
jx <jx@jvxiang.com>
2021-04-21 17:36:44 +0800

    添加图片处理组件

---
+++ Jx.Tools/Components/ImageResizeCompenent.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Jx.Tools.Components
+{
+    public partial class ImageResizeCompenent: BootstrapComponentBase
+    {
+        private ElementReference Resize { get; set; }
+        
+        private JSInterop<ImageResizeCompenent> Interop { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                if (Interop == null)
+                {
+                    Interop = new JSInterop<ImageResizeCompenent>(JSRuntime);
+                    await Interop.InvokeVoidAsync(this, Resize, "crop");
+                }
+            }
+        }
+    }
+}
---
+++ Jx.Tools/wwwroot/js/crop.js
@@ -0,0 +1,8 @@
+﻿(function ($) {
+    $.extend({
+        crop: function (el, method, param1, param2) {
+            var $el = $(el);
+            $el.cropper();
+        }
+    })
+})(jQuery);
